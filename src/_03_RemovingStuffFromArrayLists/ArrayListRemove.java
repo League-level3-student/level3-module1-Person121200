5cb55b52409edad06d9a27efaf94e2a5018a9f20
league <league@imac-28.attlocal.net>
2025-05-18 11:30:49 -0700

    GO BACKWARDS WORKIN ON ARRAYLIIST REMOVE

--- src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
+++ src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
@@ -23,7 +23,11 @@ public class ArrayListRemove {
     // 1. Write a method that removes the dirt in the yard and returns the
     //    ArrayList
     public static ArrayList<Stuff> cleanOutTheYard( ArrayList<Stuff> yard ) {
-        
+        for(int i = 0; i<yard.size(); i++) {
+        	if(yard.get(i).type.equals("dirt")) {
+        		yard.remove(i);
+        	}
+        }
         return yard;
     }
     
