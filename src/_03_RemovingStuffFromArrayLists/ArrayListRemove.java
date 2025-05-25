f389389af2c5a90be5d640b5aa0194495cb3cca1
league <league@imac-28.attlocal.net>
2025-05-25 11:26:27 -0700

    Card game IP

--- src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
+++ src/_03_RemovingStuffFromArrayLists/ArrayListRemove.java
@@ -4,36 +4,44 @@ import java.util.ArrayList;
 
 public class ArrayListRemove {
 
-    class Stuff {
-        public String type;
-    }
-    
-    class Worm extends Stuff {
-        public Worm() {
-            type = "worm";
-        }
-    }
-    
-    class Dirt extends Stuff {
-        public Dirt() {
-            type = "dirt";
-        }
+	class Stuff {
+		public String type;
+	}
 
-    // 1. Write a method that removes the dirt in the yard and returns the
-    //    ArrayList
-    public static ArrayList<Stuff> cleanOutTheYard( ArrayList<Stuff> yard ) {
-        for(int i = yard.size; i<=0; i--) {
-        	if(yard.get(i).type.equals("dirt")) {
-        		yard.remove(i);
-        	}
-        }
-        return yard;
-    }
-    
-    // 2. Write a method that removes the hash tag ('#') characters from the
-    //    ArrayList and returns it
-    public static ArrayList<Character> removeHashTags(ArrayList<Character> list) {
+	class Worm extends Stuff {
+		public Worm() {
+			type = "worm";
+		}
+	}
 
-        return list;
-    }
+	class Dirt extends Stuff {
+		public Dirt() {
+			type = "dirt";
+		}
+
+		// 1. Write a method that removes the dirt in the yard and returns the
+		// ArrayList
+
+		
+	}
+public static ArrayList<Stuff> cleanOutTheYard(ArrayList<Stuff> yard) {
+			for (int i = yard.size()-1; i >= 0; i--) {
+				if (yard.get(i).type.equals("dirt")) {
+					yard.remove(i);
+			}
+			
+			}
+			return yard;
+
+		}
+
+public static ArrayList<Character> removeHashTags(ArrayList<Character> truth) {
+	for(int i = truth.size()-1; i>=0; i--) {
+		if(truth.get(i).equals('#')) {
+			truth.remove(i);
+		}
+	}
+		return truth;
 }
+}
+
--- src/_04_Memory_Match/GameBoard.java
+++ src/_04_Memory_Match/GameBoard.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,9 +19,10 @@ public class GameBoard extends JFrame implements ActionListener {
     static Card secondSelectedCard = null;
     
     // 1. Initialize TOTAL_CARDS to 2;
-    static int TOTAL_CARDS = 0;
+    static int TOTAL_CARDS = 52;
     
     ArrayList<Card> cards;
+    ArrayList<Card> objects = new ArrayList<Card>(TOTAL_CARDS);
     
     JPanel panel;
     JLabel timeLabel;
@@ -42,34 +44,41 @@ public class GameBoard extends JFrame implements ActionListener {
         }
         
         // 2. Initialize the ArrayList of Cards declared above
-        
+        cards = new ArrayList<Card>();
         
         // 3. Create TOTAL_CARDS number of objects each with a value of 1.
         //    Also, add action listeners to each Card object and then add each
         //    of the Card objects to the ArrayList of Cards.
-        
+        for(int i = 0; i<TOTAL_CARDS; i++) {
+        	cards.add(new Card(1));
+        	cards.get(i).addActionListener(this);
+        }
         
         // 4. Use Collections.shuffle() method to randomize the order of
         //    the cards in the ArrayList
-        
+        Collections.shuffle(cards);
         
         // 5. Initialize the panel variable declared above
-        
+        panel = new JPanel();
         
         // 6. Add all of the Card objects to the panel
-        
-        
+        for(int i = 0; i<cards.size(); i++) {
+        	panel.add(cards.get(i));
+        }
         // 7. Call the setupGui() method to set up the frame
-        
+        setupGui(cards);
         
         // 8. Call the startGame() method to start the game
-        
+        startGame();
     }
 
     // 9. Fill in the drawCards method to draw all the cards in the ArrayList.
     //    Run your code and verify 2 cards are displayed and the game works.
     public void drawCards() {
-        
+        for(int i = 0; i<cards.size(); i++) {
+        	cards.get(i).draw();
+        	cards.get(i).setFaceUpIcon(Card.cardImagesPath + (i+1) + ".png");
+        }
     }
     
     // 10. 
